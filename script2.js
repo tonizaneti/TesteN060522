fcdb09559c47ff4d860dfbde835beda0060c42ee
tonizaneti <antonio.zaneti@gmail.com>
2022-05-06 09:27:26 -0300

    Primeiro Commit

---
+++ script2.js
@@ -0,0 +1,11 @@
+const meuArray = [30,30,40,5,223,2049,3034,5];
+
+function valoresUnicos(arr){
+    const mySet = new Set(arr)
+
+    //Utilizando argumento hash para converter em array
+    return [...mySet];
+}
+
+console.log(meuArray);
+console.log(valoresUnicos(meuArray));
